3b5d3856d3c5db21c3479b85ed571c44f3dce5d2
Akhil Pekala <134627876+Akhil-Ak-04@users.noreply.github.com>
2026-04-13 11:35:43 +0000

    restored

---
+++ python_basics_akhil_pekala.py
@@ -0,0 +1,16 @@
+name = 'Akhil'
+age = 25
+height_in_meters = 1.71
+is_student = False
+
+print("Name:",name,"| Age:",age,"| Height:",height_in_meters,"| Student:",is_student)
+
+age_in_months = age*12
+age_in_days = age*365
+remainder_for_seven = age%7
+age_square = age^2
+
+print("Age in Months :",age_in_months)
+print("Age in Days :",age_in_days)
+print("The remainder when age is divided by 7 :",remainder_for_seven)
+print("Age squared :",age_square)
